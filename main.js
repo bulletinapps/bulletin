4b657acbc8bb186350162ab4c8ff48139f2283ca
Dawid <dawidwas155@gmail.com>
2024-03-04 09:11:23 -0600

    aaaa

--- main.js
+++ main.js
@@ -26,8 +26,11 @@ onAuthStateChanged(auth, function(user){
         window.location.href = "index.html"
     }
 })
+//-------------------------------Global Variables-------------------------------\\
+let lastNote = null
+let selectedNote = [null, null] // [0] = note id, [1] = note element
+let currentColor = "rgb(255, 238, 153)"
 //-------------------------------Elements-------------------------------\\
-let selectedNote = [null, null]
 const board = document.getElementById("board")
 const signoutButton = document.getElementById("logoutButton")
 
@@ -38,6 +41,7 @@ const addNoteButton = document.getElementById("addNoteButton")
 
 const editNoteInputBox = document.getElementById("editNoteInputBox")
 const editNoteButton = document.getElementById("editNoteButton")
+const deleteNoteButton = document.getElementById("deleteNoteButton")
 
 const darkModeSwitch = document.getElementById("darkModeCheck")
 const autoScrollSwtich = document.getElementById("autoScrollCheck")
@@ -47,112 +51,7 @@ const updateEmailButton = document.getElementById("updateEmailButton")
 const updatePasswordButton = document.getElementById("updatePasswordButton")
 const usernameInput = document.getElementById("usernameInput")
 const emailInput = document.getElementById("emailInput")
-//-------------------------------Update Information-------------------------------\\
-updateUsernameButton.addEventListener("click", function(){
-    if(usernameInput.value != "" && auth.currentUser != null){
-        update(ref(db, "users/" + auth.currentUser.uid),{
-            username: usernameInput.value
-        })
-    }
-})
-
-updateEmailButton.addEventListener("click", function(){
-    if(emailInput.value != "" && auth.currentUser != null){
-        update(ref(db, "users/" + auth.currentUser.uid),{
-            email: emailInput.value 
-        }).then(function(){
-            updateEmail(auth.currentUser, emailInput.value).then(function(){
-                alert("Successfully updated user's email!")
-            }).catch(function(err){
-                alert("An error has occured updating the user's email in the authentication system. Try again. Error: " + err)
-            })
-        }).catch(function(err){
-            alert("Error saving the user's email to the database. Try again. Error: " + err)
-        })
-    }
-})
-
-updatePasswordButton.addEventListener("click", function(){
-    if(auth.currentUser != null){
-        sendPasswordResetEmail(auth, auth.currentUser.email)
-    }
-})
-//---------------------Logout---------------------\\
-signoutButton.addEventListener("click", function(){
-    auth.signOut()
-    window.location.href = "index.html"
-})
-//---------------------DarkMode---------------------\\
-darkModeSwitch.addEventListener("click", function(){
-    if(darkModeSwitch.checked == true){
-        document.querySelector("html").setAttribute("data-bs-theme", "dark")
-    } else{
-        document.querySelector("html").setAttribute("data-bs-theme", "light")
-    }
-})
-//---------------------Colors---------------------\\
-const colors = [
-    "rgb(234, 31, 31)",
-    "rgb(234, 153, 153)",
-    "rgb(255, 147, 0)",
-    "rgb(255, 213, 86)",
-    "rgb(255, 238, 0)",
-    "rgb(255, 238, 153)",
-    "rgb(58, 190, 0)",
-    "rgb(147, 196, 125)",
-    "rgb(0, 134, 255)",
-    "rgb(111, 168, 220)",
-    "rgb(247, 51, 148)",
-    "rgb(255, 162, 211)",
-    "rgb(103, 43, 255)",
-    "rgb(142, 124, 195)",
-    "rgb(48, 42, 42)",
-    "rgb(243, 246, 244)",   
-    "rgb(142, 108, 56)",
-    "rgb(255, 229, 180)"
-]
-
-const customTextColors = {
-    "rgb(48, 42, 42)": "rgb(255, 255, 255)",
-    "rgb(103, 43, 255)": "rgb(255, 255, 255)"
-}
-
-let currentColor = "rgb(255, 238, 153)"
-let selectedElement = null
-for(let i = 0; i < colors.length; i++){
-    let colorChoice = document.createElement("div")
-    colorChoice.classList.add("colorChoice")
-    colorChoice.style.backgroundColor = colors[i]
-    colorChoice.id = colors[i]
-    colorBox.appendChild(colorChoice)
-
-    let selectedIcon = document.createElement("i")
-    selectedIcon.classList.add("bi-check2")
-    selectedIcon.classList.add("selectedIcon")
-
-    if(colors[i] == currentColor){
-        selectedIcon.style.visibility = "visible"
-        selectedElement = colorChoice
-    } else{
-        selectedIcon.style.visibility = "hidden"
-    }
-    if(customTextColors[colors[i]] != undefined){
-        selectedIcon.style.color = customTextColors[colors[i]]
-    } else{
-        selectedIcon.style.color = "rgb(0,0,0)"
-    }
-    colorChoice.appendChild(selectedIcon)
-
-    colorChoice.addEventListener("click", function(){
-        if(selectedElement != null){
-            selectedElement.childNodes[0].style.visibility = "hidden"
-        }
-        currentColor = colors[i]
-        selectedElement = colorChoice
-        selectedElement.childNodes[0].style.visibility = "visible"
-    })
-} 
-//-------------------------------Adds Notes-------------------------------\\
+//-------------------------------Functions-------------------------------\\
 function addNote(id, text, color){
     //------------Note Creation------------\\
     let note = document.createElement("div")
@@ -200,7 +99,7 @@ function editNote(id, noteElement, newText, newColor){
     }).then(function(){
         //------------Text------------\\
         noteElement.innerHTML = newText
-        noteElement.style.setProperty("--fontSize", Math.min(35, Math.max((7/text.length)*105, 9)) + "px")
+        noteElement.style.setProperty("--fontSize", Math.min(35, Math.max((7/newText.length)*105, 9)) + "px")
         //------------Color------------\\
         if(newColor != null){
             noteElement.style.backgroundColor = newColor
@@ -227,22 +126,7 @@ function editNote(id, noteElement, newText, newColor){
         alert("An error has occurred trying to save your note. Try again. Error: " + err)
     })
 }
-//-------------------------------Uploads Messages-------------------------------\\
-addNoteButton.addEventListener("click", function(){
-    if (addNoteInputBox.value != "" && auth.currentUser != null){
-        const boardRef = ref(db, "users/" + auth.currentUser.uid + "/board")
-        const pushBoardRef = push(boardRef)
-        set(pushBoardRef,{ 
-            text: addNoteInputBox.value,
-            color: currentColor
-        }).then(function(){}).catch(function(err){
-            alert("An error has occurred trying to save your note. Try again. Error: " + err)
-        })
-        addNoteInputBox.value = ""
-    }
-})
-//---------------------Loads Messages---------------------\\
-let lastNote = null
+
 window.addEventListener("load", function(){
     setTimeout(function(){
         if(auth.currentUser == null){
@@ -267,9 +151,141 @@ window.addEventListener("load", function(){
         }
     }, 1000)
 })
+//---------------------Colors---------------------\\
+const colors = [
+    "rgb(234, 31, 31)",
+    "rgb(234, 153, 153)",
+    "rgb(255, 147, 0)",
+    "rgb(255, 213, 86)",
+    "rgb(255, 238, 0)",
+    "rgb(255, 238, 153)",
+    "rgb(58, 190, 0)",
+    "rgb(147, 196, 125)",
+    "rgb(0, 134, 255)",
+    "rgb(111, 168, 220)",
+    "rgb(247, 51, 148)",
+    "rgb(255, 162, 211)",
+    "rgb(103, 43, 255)",
+    "rgb(142, 124, 195)",
+    "rgb(48, 42, 42)",
+    "rgb(243, 246, 244)",   
+    "rgb(142, 108, 56)",
+    "rgb(255, 229, 180)"
+]
+
+const customTextColors = {
+    "rgb(48, 42, 42)": "rgb(255, 255, 255)",
+    "rgb(103, 43, 255)": "rgb(255, 255, 255)"
+}
+
+let selectedElement = null
+for(let i = 0; i < colors.length; i++){
+    let colorChoice = document.createElement("div")
+    colorChoice.classList.add("colorChoice")
+    colorChoice.style.backgroundColor = colors[i]
+    colorChoice.id = colors[i]
+    colorBox.appendChild(colorChoice)
+
+    let selectedIcon = document.createElement("i")
+    selectedIcon.classList.add("bi-check2")
+    selectedIcon.classList.add("selectedIcon")
+
+    if(colors[i] == currentColor){
+        selectedIcon.style.visibility = "visible"
+        selectedElement = colorChoice
+    } else{
+        selectedIcon.style.visibility = "hidden"
+    }
+    if(customTextColors[colors[i]] != undefined){
+        selectedIcon.style.color = customTextColors[colors[i]]
+    } else{
+        selectedIcon.style.color = "rgb(0,0,0)"
+    }
+    colorChoice.appendChild(selectedIcon)
+
+    colorChoice.addEventListener("click", function(){
+        currentColor = colors[i]
+        if(selectedElement != null){
+            selectedElement.childNodes[0].style.visibility = "hidden"
+        }
+        currentColor = colors[i]
+        selectedElement = colorChoice
+        selectedElement.childNodes[0].style.visibility = "visible"
+    })
+} 
+//-------------------------------Buttons-------------------------------\\
+addNoteButton.addEventListener("click", function(){
+    if (addNoteInputBox.value != "" && auth.currentUser != null){
+        const boardRef = ref(db, "users/" + auth.currentUser.uid + "/board")
+        const pushBoardRef = push(boardRef)
+        set(pushBoardRef,{ 
+            text: addNoteInputBox.value,
+            color: currentColor
+        }).then(function(){}).catch(function(err){
+            alert("An error has occurred trying to save your note. Try again. Error: " + err)
+        })
+        addNoteInputBox.value = ""
+    }
+})
+
+updateUsernameButton.addEventListener("click", function(){
+    if(usernameInput.value != "" && auth.currentUser != null){
+        update(ref(db, "users/" + auth.currentUser.uid),{
+            username: usernameInput.value
+        })
+    }
+})
+
+updateEmailButton.addEventListener("click", function(){
+    if(emailInput.value != "" && auth.currentUser != null){
+        update(ref(db, "users/" + auth.currentUser.uid),{
+            email: emailInput.value 
+        }).then(function(){
+            updateEmail(auth.currentUser, emailInput.value).then(function(){
+                alert("Successfully updated user's email!")
+            }).catch(function(err){
+                alert("An error has occured updating the user's email in the authentication system. Try again. Error: " + err)
+            })
+        }).catch(function(err){
+            alert("Error saving the user's email to the database. Try again. Error: " + err)
+        })
+    }
+})
+
+updatePasswordButton.addEventListener("click", function(){
+    if(auth.currentUser != null){
+        sendPasswordResetEmail(auth, auth.currentUser.email)
+    }
+})
+
+signoutButton.addEventListener("click", function(){
+    auth.signOut()
+    window.location.href = "index.html"
+})
+
+darkModeSwitch.addEventListener("click", function(){
+    if(darkModeSwitch.checked == true){
+        document.querySelector("html").setAttribute("data-bs-theme", "dark")
+    } else{
+        document.querySelector("html").setAttribute("data-bs-theme", "light")
+    }
+})
 
 autoScrollSwtich.addEventListener("click", function(){
     if(autoScrollSwtich.checked == true && lastNote != null){
         lastNote.scrollIntoView({behavior: "smooth"})
+    }
+})
+
+editNoteButton.addEventListener("click", function(){
+    if(selectedNote != [null, null] && editNoteInputBox.value != ""){
+        editNote(selectedNote[0], selectedNote[1], editNoteInputBox.value, currentColor)
+        editNoteInputBox.value = ""
+    }
+})
+
+deleteNoteButton.addEventListener("click", function(){
+    if(selectedNote != [null, null] ){
+        // TBA
     }
 })
